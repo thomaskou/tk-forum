af4ee618f7f08be17e88352ade7aed00df28a1c2
Thomas Kou <thomaskou00@gmail.com>
2019-01-18 18:23:18 -0500

    Updated users module.

--- models/user.js
+++ models/user.js
@@ -2,28 +2,84 @@ const db = require('../database');
 
 var User = {};
 
-User.newUser = function(name, email, pass) {
+User.getInfo = function(name, res) {
+    var sql = "SELECT * FROM users WHERE name = '"+name+"'";
+    db.query(sql, function(err, result) {
+        if (err) throw err;
+        //console.log(result[0]);
+        res.send(result[0]);
+    });
+    res.end();
+};
+
+User.newUser = function(name, email, pass, res) {
     db.query("SELECT COUNT(*) AS count FROM users", function(err, result) {
         if (err) throw err;
         var id = result[0].count;
         db.query("SELECT SHA2('"+pass+"', 512) AS sha", function(err, result) {
             if (err) throw err;
             var pass_sha512 = result[0].sha;
-            var sql = "INSERT INTO users VALUES ('"+id+"','"+name+"','"+email+"', '"+pass_sha512+"')";
+            var sql = "INSERT INTO users VALUES ('"+id+"','"+name+"','"+email+"','"+pass_sha512+"')";
             db.query(sql, function(err, result) {
                 if (err) throw err;
-                console.log("Successfully added 1 user to database.");
+                res.send("Successfully added 1 user to database.")
+                //console.log("Successfully added 1 user to database.");
             });
         })
     });
+    res.end();
 };
 
-User.getInfo = function(name) {
-    var sql = "SELECT * FROM users WHERE name = '"+name+"'";
+User.updateUser = function(name, email, pass, res) {
+    if (typeof name !== 'undefined') {
+        /*
+        var sql = "UPDATE users SET name = '"+name+"' WHERE name = '"+name+"'";
+        db.query(sql, function(err, result) {
+            if (err) throw err;
+            console.log("Successfully updated one username.");
+        });
+        */
+        if (typeof email !== 'undefined') {
+            var sql = "UPDATE users SET email = '"+email+"' WHERE name = '"+name+"'";
+            db.query(sql, function(err, result) {
+                if (err) throw err;
+                res.send("Successfully updated one email.");
+                //console.log("Successfully updated one email.");
+            });
+        }
+        if (typeof pass !== 'undefined') {
+            db.query("SELECT SHA2('"+pass+"', 512) AS sha", function(err, result) {
+                if (err) throw err;
+                var pass_sha512 = result[0].sha;
+                var sql = "UPDATE users SET pass_sha512 = '"+pass_sha512+"' WHERE name = '"+name+"'";
+                db.query(sql, function(err, result) {
+                    if (err) throw err;
+                    res.send("Successfully updated one password.");
+                    //console.log("Successfully updated one password.");
+                });
+            })
+        }
+    }
+    res.end();
+};
+
+User.deleteUser = function(name, res) {
+    var sql = "DELETE FROM users WHERE name = '"+name+"'";
     db.query(sql, function(err, result) {
         if (err) throw err;
-        console.log(result);
+        res.send("Successfully deleted one user.");
+        //console.log("Successfully deleted one user.");
     });
-};
+    res.end();
+}
+
+User.getAll = function(res) {
+    var sql = "SELECT * FROM users";
+    db.query(sql, function(err, result) {
+        if (err) throw err;
+        res.send(result);
+    });
+    res.end();
+}
 
 module.exports = User;
--- routes/users.js
+++ routes/users.js
@@ -1,23 +1,30 @@
 const User = require('../models/user');
 
+//Gets a JSON with a list of information regarding all users.
 exports.user_all = function(req, res) {
-    res.send("NOT IMPLEMENTED: GET /api/users");
+    User.getAll(res);
 };
 
+//Adds a new user to the database with automatic ID generation.
+//Usage: body:user, body:email, body:pass
 exports.user_new = function(req, res) {
-    User.newUser(req.body.user, req.body.email, req.body.pass);
-    res.end();
+    User.newUser(req.body.user, req.body.email, req.body.pass, res);
 };
 
+//Gets a JSON with a user's information (finds using username).
+//Usage: params:name
 exports.user_get = function(req, res) {
-    User.getInfo(req.params.name);
-    res.end();
+    User.getInfo(req.params.name, res);
 };
 
+//Updates a user's information (finds using username).
+//Usage: params:name, body:email, body:pass
 exports.user_update = function(req, res) {
-    res.send("NOT IMPLEMENTED: PUT /api/users/:id");
+    User.updateUser(req.params.name, req.body.email, req.body.pass, res);
 };
 
+//Deletes a user from the database (finds using username).
+//Usage: params:name
 exports.user_delete = function(req, res) {
-    res.send("NOT IMPLEMENTED: DELETE /api/users/:id");
+    User.deleteUser(req.params.name, res);
 };
